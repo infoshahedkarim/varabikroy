9bd7ea292e9950542fdeb303fe7e04202415031f
Shahed Karim <info.shahedkarim@gmail.com>
2025-06-18 12:33:47 +0600

    bugfix

--- resources/views/frontend/tolet.blade.php
+++ resources/views/frontend/tolet.blade.php
@@ -8,22 +8,22 @@ To-Let |
 
 @section('content')
 
- <section class="banner-two position-relative overflow-hidden py-5 m-0">
-        <div class="swiper banner-swiper position-relative w-100" style="line-height: 0;">
-            <div class="swiper-wrapper">
-                @foreach ($banners as $banner)
-                    <div class="swiper-slide">
-                            <img src="{{ asset('storage/' . $banner->img) }}" alt="Banner Light" class="slide-image">
-                        </div>
-                @endforeach
+<section class="banner-two position-relative overflow-hidden py-5 m-0">
+    <div class="swiper banner-swiper position-relative w-100" style="line-height: 0;">
+        <div class="swiper-wrapper">
+            @foreach ($banners as $banner)
+            <div class="swiper-slide">
+                <img src="{{ asset('storage/' . $banner->img) }}" alt="Banner Light" class="slide-image">
             </div>
-
-            <!-- Controls -->
-            <div class="swiper-pagination"></div>
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
+            @endforeach
         </div>
-    </section>
+
+        <!-- Controls -->
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+    </div>
+</section>
 
 
 <section class="service-three position-relative section-bg padding-y-120 z-index-1">
@@ -58,7 +58,23 @@ To-Let |
 
 
 
-
+<script>
+    var swiper = new Swiper(".banner-swiper", {
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+        }
+    });
+</script>
 
 
 @endsection
