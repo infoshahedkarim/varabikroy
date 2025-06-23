e158028584b77a71c8ad4d8dab83dad4ca3751fd
Shahed Karim <info.shahedkarim@gmail.com>
2025-06-23 20:29:30 +0600

    sitemap add

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -5,16 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Search properties for sale and rent in Bangladesh on Varabikroy.com.">
+    <meta name="description" content="">
     <meta name="keywords" content="varabikroy,buy, sell, property, vara, varabikroy.com, bikroy.com, bikroy flat, bikroy house, bikroy land, bikroy apartment, bikroy commercial, bikroy shop, bikroy office, bikroy rent, bikroy sale, buy property in Bangladesh, sell property in Bangladesh, rent property in Bangladesh, real estate in Bangladesh">
     <meta name="author" content="@shahedkarim">
     <meta name="google-site-verification" content="l_E4df4AKzdssNvrj_GvZ326rskSlvzgKqXajTeokL4" />
     <meta name="robots" content="index, follow">
-    <title> @yield('title') varabikroy.com</title>
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.ico') }}">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-    <link rel="icon" href="{{ asset('assets/images/logo/favicon.ico') }}" sizes="any">
+    <title>Search properties for sale and rent in Bangladesh on Varabikroy.com</title>
+    <link rel="icon" href="{{ asset('assets/images/logo/favicon.ico') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+
+    <link href="{{ asset('assets/images/logo/favicon.ico') }}" rel="icon" sizes="any">
     <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+
     <link rel="apple-touch-icon" href="{{ asset('assets/images/logo/favicon.ico') }}">
     <link rel="manifest" href="{{ asset('assets/images/logo/favicon.ico') }}">
 
